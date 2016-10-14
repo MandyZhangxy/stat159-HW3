8c4dfd2e207698b9ab77a1eee78994f171b15772
MandyZhangxy <mandy.zhang512@gmail.com>
2016-10-14 00:26:40 -0700

    done

--- code/README.md
+++ code/README.md
@@ -13,5 +13,5 @@ In this folder, it has one `test-that.R` file and three sub-folders: `functions/
 	* `regression-script.R`: it reads in the `Advertising.csv` data set, and computes a `"regression"` object---via `lm()`---as well as the summary of such regression object---via `summary()`. These objects are saved in the file `regression.RData`. This script also produces the three diagnostics plots `residual-plot.png`, `scale-location-plot.png`, and `normal-qq-plot.png`.  
 	* `session-info-script.R`: This is a script that includes `library()` calls to ALL the packages that you use for your project, as well as the output of the function `sessionInfo()`. Export the output via `sink()` to the file `session-info.txt`
 	
--`tests/`: this folder has a `test-regression.R` file which is for testing purposes, using the data `mtcars` (that comes in R), and computing a multiple regression of `mpg` on `disp` and `hp`. 
+*	`tests/`: this folder has a `test-regression.R` file which is for testing purposes, using the data `mtcars` (that comes in R), and computing a multiple regression of `mpg` on `disp` and `hp`. 
 
--- code/tests/test-regression.R
+++ code/tests/test-regression.R
@@ -4,7 +4,7 @@ library(testthat)
 #test the residual_sum_squares function
 context("Test for residual_sum_squares function")
 test_that("RSS as expected", {
-  reg = lm(mpg ~ disp + hp + wt, data = mtcars)
+  reg = lm(mpg ~ disp + hp, data = mtcars)
   reg_sum = summary(reg)
   expect_equal(residual_sum_squares(reg), sum(reg$residuals^2)) 
   expect_length(residual_sum_squares(reg),1)
@@ -14,7 +14,7 @@ test_that("RSS as expected", {
 #test the total_sum_squares function
 context("Test for total_sum_squres function")
 test_that("TSS as expected", {
-  reg = lm(mpg ~ disp + hp + wt, data = mtcars)
+  reg = lm(mpg ~ disp + hp, data = mtcars)
   reg_sum = summary(reg)
   y = reg$model[[1]]
   expect_equal(total_sum_squares(reg), sum((y-mean(y))^2)) 
@@ -26,7 +26,7 @@ test_that("TSS as expected", {
 #test the r_square function
 context("Test for r_square function")
 test_that ("r square as expected", {
-  reg = lm(mpg ~ disp + hp + wt, data = mtcars)
+  reg = lm(mpg ~ disp + hp, data = mtcars)
   reg_sum = summary(reg)
   expect_equal(r_squared(reg),reg_sum$r.squared)
   expect_length(r_squared(reg),1)
@@ -36,7 +36,7 @@ test_that ("r square as expected", {
 #test the F-statistic function
 context("Test for F-statistic function")
 test_that ("F-statistic as expected", {
-  reg = lm(mpg ~ disp + hp + wt, data = mtcars)
+  reg = lm(mpg ~ disp + hp, data = mtcars)
   reg_sum = summary(reg)
   expect_equal(f_statistic(reg), reg_sum$fstatistic[[1]])
   expect_type(f_statistic(reg),'double')
@@ -45,7 +45,7 @@ test_that ("F-statistic as expected", {
 #test the residual_std_error function
 context("Test for residual_std_error function")
 test_that ("RSE as expected", {
-  reg = lm(mpg ~ disp + hp + wt, data = mtcars)
+  reg = lm(mpg ~ disp + hp, data = mtcars)
   reg_sum = summary(reg)
   expect_equal(residual_std_error(reg), reg_sum$sigma)
   expect_length(residual_std_error(reg),1)
